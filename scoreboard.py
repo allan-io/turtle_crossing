411c933b75dee1f689af92eb4dbd479fe6ed3b2c
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-16 13:40:23 -0300

    Add increase_level method to scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -10,13 +10,16 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(-280, 250)
-        self.update_scoreboeard()
+        self.update_scoreboard()
 
-    def update_scoreboeard(self):
+    def update_scoreboard(self):
+        self.clear()
         self.write(f"Level {self.level}", align="left", font=FONT)
 
-
     def increase_level(self):
-        self.clear()
         self.level += 1
-        self.update_scoreboeard()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align="center", font=FONT)
